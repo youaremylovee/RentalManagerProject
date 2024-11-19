18004bc40cb432eddbb2c4379668bd4a7d3f1c7d
Nguyen Dan Truong <dantruong.congviec@gmail.com>
2024-11-19 12:34:22 +0700

    Update PathConfig.java

--- src/Config/PathConfig.java
+++ src/Config/PathConfig.java
@@ -1,5 +1,5 @@
 package Config;
 
 public class PathConfig {
-    public static final String OUT_DIR = "C:\\Users\\Nguyen Dan Truong\\Desktop\\Github\\RentalManager\\src\\Resources\\";
+    public static final String OUT_DIR = "C:\\Users\\Nguyen Dan Truong\\Desktop\\RentalManager\\src\\Resources\\";
 }
